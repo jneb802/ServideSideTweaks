33dc2a6cd30be060a9419d3ef01a92aa7b4927c8
jneb802 <73610029+jneb802@users.noreply.github.com>
2026-05-17 15:23:34 -0400

    Bump server side tweaks version to 1.1.1

--- Source/Plugin.cs
+++ Source/Plugin.cs
@@ -17,7 +17,7 @@ namespace ServerSideTweaks
     public class ServerSideTweaksPlugin : BaseUnityPlugin
     {
         private const string ModName = "serverSideTweaks";
-        private const string ModVersion = "1.1.0";
+        private const string ModVersion = "1.1.1";
         private const string ModGUID = "warpalicious.serverSideTweaks";
 
         private readonly Harmony _harmony = new(ModGUID);
